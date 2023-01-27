92f56fc4bfb78d34f2d2a5a8a9e7f57f248648df
Karol Jędrzejewski <karol.jedrzejewski10@gmail.com>
2023-01-27 22:57:39 +0100

    cart finalization

--- cart/cart.py
+++ cart/cart.py
@@ -36,6 +36,21 @@ class Cart():
 
         self.session.modified = True
 
+    
+    
+    def delete(self, product):
+        
+        product_id = str(product)
+
+        if product_id in self.cart:
+
+            del self.cart[product_id]
+
+        self.session.modified = True
+
+
+        
+
     def __len__(self):
 
         return sum(item['qty'] for item in self.cart.values())
@@ -63,3 +78,15 @@ class Cart():
     def get_total(self):
 
         return sum(Decimal(item['price']) * item['qty'] for item in self.cart.values())
+
+
+    def update(self, product, qty):
+
+        product_id = str(product)
+        product_quantity = qty
+
+        if product_id in self.cart:
+
+            self.cart[product_id]['qty'] = product_quantity
+
+        self.session.modified = True
